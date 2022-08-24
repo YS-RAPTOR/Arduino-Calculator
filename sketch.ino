9b8a83df787e01c1ebda534a9a8833585eba16ec
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2022-08-24 22:16:34 +1000

    modified:   Calculator.cpp 	modified:   Calculator.h 	diagram.json

--- sketch.ino
+++ sketch.ino
@@ -1,19 +1,43 @@
+#include <Keypad.h>
+#include <LiquidCrystal.h>
+
 /* Code to Test Calculator Calculate Function */
+
+// TODO Fix -+-+-+ Error
+// TODO Fix - can be entered when off
 
 #include "Calculator.h"
 
+// Keypad initializing
+const byte KEYPAD_ROWS = 4;
+const byte KEYPAD_COLS = 4;
+byte rowPins[KEYPAD_ROWS] = {9, 8, 7, 6};
+byte colPins[KEYPAD_COLS] = {5, 4, 3, 2};
+char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
+    {'1', '2', '3', '*'},
+    {'4', '5', '6', '-'},
+    {'7', '8', '9', '+'},
+    {'o', '0', '.', '='}};
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
+
+LiquidCrystal lcd(14, 15, 13, 12, 11, 10);
+
 Calculator calc;
-String expression;
 
 void setup() {
-    // put your setup code here, to run once:
+    // Start serial monitor
     Serial.begin(9600);
-    Serial.println("Enter Expression to Solve");
+
+    // lcd
+    lcd.begin(16, 2);
+
+    calc.begin(&lcd, &keypad);
+
+    pinMode(16, OUTPUT);
+    digitalWrite(16, HIGH);
+    digitalWrite(16, LOW);
 }
 
 void loop() {
-    if (Serial.available() > 0) {
-        expression = Serial.readString();
-        Serial.println(calc.Calculate(expression));
-    }
+    calc.PollInput();
 }
